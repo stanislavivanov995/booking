426b0314f30f007b7e8ea7192cc4f243ce84dfbd
Kosta Kazakov <kostakazakoff@gmail.com>
2024-01-08 21:12:20 +0200

    CategorySeeder added to AppServiceProvider boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
+
     public function boot(): void
     {
-        //
+        if (Schema::hasTable('categories')) {
+            Artisan::call('db:seed', ['--class' => 'CategorySeeder']);
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,17 +1,16 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Category;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    
     public function run(): void
-    {
+    {        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
