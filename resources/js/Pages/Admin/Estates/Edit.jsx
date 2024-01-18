599e9b1dd59f50ebc443d814c3a0364a191ddd97
da20shadow <92220876+da20shadow@users.noreply.github.com>
2024-01-18 21:38:50 +0200

    update edit page

--- resources/js/Pages/Admin/Estates/Edit.jsx
+++ resources/js/Pages/Admin/Estates/Edit.jsx
@@ -1,22 +1,25 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import {Head} from '@inertiajs/react';
+import EditEstateForm from "@/Components/EditEstateForm.jsx";
 
 
-export default function Edit({ auth, estate }) {
-  return (
-    <AuthenticatedLayout
-      user={auth.user}
-      header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Estate: {estate.id}</h2>}
-    >
-      <Head title={'Estate:' + estate.name} />
+export default function Edit({auth, estate}) {
+    console.log(estate)
+    console.log(estate.name)
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">{estate.name}</h2>}
+        >
+            <Head title={'Estate:' + estate.name}/>
 
-      {/* <div className="py-12">
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            
-          </div>
-        </div>
-      </div> */}
-    </AuthenticatedLayout>
-  );
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                    <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                        <EditEstateForm estate={estate}/>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
 }
