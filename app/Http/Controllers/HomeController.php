4f9a1d4cb17524958b57e36ce3df2166f311c821
Kosta Kazakov <kostakazakoff@gmail.com>
2024-01-29 20:44:30 +0200

    filter disabled estates in results

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -75,6 +75,7 @@ class HomeController extends Controller
         }
 
         $estates = Estate::select('*')
+        ->where('is_disabled', '0')
         ->selectRaw(
             '(6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance',
             [$latitude, $longitude, $latitude]
